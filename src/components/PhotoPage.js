c8fbe3ac30d376d0ab2243e7104d930f13fed5ab
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-19 14:02:03 -0600

    Installed ReactStrap and Styled-Components

--- src/components/PhotoPage.js
+++ src/components/PhotoPage.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Container, Row, Col, Collapse} from 'reactstrap';
 
 const PhotoPage = props =>{
+    const [visible, setVisible] = useState(false);
+    const toggle = () => setVisible(!visible);
    
   // document.querySelector('.mainPhoto').style.backgroundImage =`url(${props.image})`;
 
     return(
-    <section className = "mainPhoto" 
-    style = {{backgroundImage: "url("+ props.image + ")" }}>
-        
-        <section className = "description">
-        <h3 className = "photo-date">
-            {props.date}
-        </h3>
-        <h1>{props.title}</h1>
-        <h2>{props.author}</h2>
-        <p>explanation: {props.explanation}</p>
-        </section>
-    </section>
+        <Container>
+            <div className = "mainPhoto" onClick = {toggle}
+            style = {{backgroundImage: "url("+ props.image + ")" }}>
+                
+                <Collapse visible={visible}>
+                    <section className = "description">
+                    <h3 className = "photo-date">
+                        {props.date}
+                    </h3>
+                    <h1>{props.title}</h1>
+                    <h2>{props.author}</h2>
+                    <p>explanation: {props.explanation}</p>
+                    </section>
+                </Collapse>
+            </div>
+        </Container>
     )
 }
 export default PhotoPage;
