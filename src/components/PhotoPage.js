623c994c0257f4d3325f964626a9506ac13cfe4e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-19 14:39:49 -0600

    Incorperating Collapsing button in PhotoPage.js

--- src/components/PhotoPage.js
+++ src/components/PhotoPage.js
@@ -1,28 +1,31 @@
 import React, {useState} from 'react';
-import {Container, Row, Col, Collapse} from 'reactstrap';
+import {Container, Col, Collapse, Card, 
+    CardBody, CardTitle, CardSubtitle, 
+    CardText, Button} from 'reactstrap';
 
 const PhotoPage = props =>{
     const [visible, setVisible] = useState(false);
     const toggle = () => setVisible(!visible);
    
-  // document.querySelector('.mainPhoto').style.backgroundImage =`url(${props.image})`;
-
     return(
         <Container>
-            <div className = "mainPhoto" onClick = {toggle}
+            <Col sm="10">
+            <Card className = "mainPhoto"
             style = {{backgroundImage: "url("+ props.image + ")" }}>
                 
-                <Collapse visible={visible}>
-                    <section className = "description">
-                    <h3 className = "photo-date">
+                <Collapse isOpen={visible}>
+                    <CardBody className = "description">
+                    <CardSubtitle className = "photo-date">
                         {props.date}
-                    </h3>
-                    <h1>{props.title}</h1>
-                    <h2>{props.author}</h2>
-                    <p>explanation: {props.explanation}</p>
-                    </section>
+                    </CardSubtitle>
+                    <CardTitle>{props.title}</CardTitle>
+                    <CardSubtitle>{props.author}</CardSubtitle>
+                    <CardText>explanation: {props.explanation}</CardText>
+                    </CardBody>
                 </Collapse>
-            </div>
+            </Card>
+            </Col>
+            <Button outline color="primary" onClick = {toggle}>Description</Button>
         </Container>
     )
 }
